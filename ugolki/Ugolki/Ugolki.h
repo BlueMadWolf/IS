d22288369ccf4ff6b7db947fe415fc5b97b82487
BlueMadWolf <sasha_kulishova@mail.ru>
2018-09-29 20:20:02 +0300

    Create struct

--- ugolki/Ugolki/Ugolki.h
+++ ugolki/Ugolki/Ugolki.h
@@ -1,2 +1,28 @@
 #pragma once
 #include <iostream>
+#include <vector>
+#include <string>
+
+std::vector<int> border(64);
+
+//структура набора шашек
+struct checkers
+{
+	checkers * parent;
+	std::vector<int> position;
+
+	checkers(std::string s, checkers * par = nullptr):parent(par)
+	{
+		for (int i = 0; i < 12; ++i)
+			position.push_back((s[i * 2] - 'A' + 1) + (s[i * 2 + 1] - '0') * 8);
+	}
+
+	checkers(std::vector<int> pos, checkers * par = nullptr) :parent(par), position(pos) {}
+
+	checkers * step(int num_check, int new_positions)
+	{
+		std::vector<int> v = position;
+		v[num_check] = new_positions;
+		return new checkers(v, this);
+	}
+};
--- ugolki/Ugolki/main.cpp
+++ ugolki/Ugolki/main.cpp
@@ -1,4 +1,29 @@
 #include "Ugolki.h"
 
+checkers * me, * rival;
+
+void InputData()
+{
+	std::cout << "Will you be player 1 or 2?";
+	int num;
+	std::cin >> num;
+
+	if (num == 1)
+	{
+		me = new checkers("A8B8C8D8A7B7C7D7A6B6C6D6");
+		rival = new checkers("H1G1F1E1H2G2F2E2H3G3F3E3");
+	}
+	else
+	{
+		{
+			rival = new checkers("A8B8C8D8A7B7C7D7A6B6C6D6");
+			me = new checkers("H1G1F1E1H2G2F2E2H3G3F3E3");
+		}
+	}
+
+}
+
 int main()
-{}
+{
+	InputData();
+}
