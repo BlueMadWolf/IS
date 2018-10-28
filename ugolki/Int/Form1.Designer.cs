555cb002fff7807809eddc53071799d1a418b626
= <rud.sasha1997@gmail.com>
2018-10-28 17:45:03 +0300

    int upd

--- ugolki/Int/Form1.Designer.cs
+++ ugolki/Int/Form1.Designer.cs
@@ -38,7 +38,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(78, 10);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(800, 800);
+            this.pictureBox1.Size = new System.Drawing.Size(600, 600);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
@@ -47,7 +47,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(919, 843);
+            this.ClientSize = new System.Drawing.Size(691, 623);
             this.Controls.Add(this.pictureBox1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
