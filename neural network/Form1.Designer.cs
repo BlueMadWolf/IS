42d94d5da5fad13b21d256374617b8d33838074a
alexander <rud.sasha1997@gmail.com>
2018-12-04 15:31:06 +0300

    nn upd

--- neural network/Form1.Designer.cs
+++ neural network/Form1.Designer.cs
@@ -29,23 +29,85 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonRectangle = new System.Windows.Forms.Button();
+            this.buttonTriangle = new System.Windows.Forms.Button();
+            this.buttonCircle = new System.Windows.Forms.Button();
+            this.buttonSin = new System.Windows.Forms.Button();
+            this.buttonRandomFig = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(176, 12);
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(235, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(696, 578);
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // buttonRectangle
+            // 
+            this.buttonRectangle.Location = new System.Drawing.Point(39, 15);
+            this.buttonRectangle.Name = "buttonRectangle";
+            this.buttonRectangle.Size = new System.Drawing.Size(126, 29);
+            this.buttonRectangle.TabIndex = 1;
+            this.buttonRectangle.Text = "drawRectangle";
+            this.buttonRectangle.UseVisualStyleBackColor = true;
+            this.buttonRectangle.Click += new System.EventHandler(this.buttonRectangle_Click);
+            // 
+            // buttonTriangle
+            // 
+            this.buttonTriangle.Location = new System.Drawing.Point(39, 61);
+            this.buttonTriangle.Name = "buttonTriangle";
+            this.buttonTriangle.Size = new System.Drawing.Size(126, 29);
+            this.buttonTriangle.TabIndex = 2;
+            this.buttonTriangle.Text = "drawTriangle";
+            this.buttonTriangle.UseVisualStyleBackColor = true;
+            this.buttonTriangle.Click += new System.EventHandler(this.buttonTriangle_Click);
+            // 
+            // buttonCircle
+            // 
+            this.buttonCircle.Location = new System.Drawing.Point(39, 109);
+            this.buttonCircle.Name = "buttonCircle";
+            this.buttonCircle.Size = new System.Drawing.Size(126, 29);
+            this.buttonCircle.TabIndex = 3;
+            this.buttonCircle.Text = "drawCircle";
+            this.buttonCircle.UseVisualStyleBackColor = true;
+            this.buttonCircle.Click += new System.EventHandler(this.buttonCircle_Click);
+            // 
+            // buttonSin
+            // 
+            this.buttonSin.Location = new System.Drawing.Point(39, 159);
+            this.buttonSin.Name = "buttonSin";
+            this.buttonSin.Size = new System.Drawing.Size(126, 29);
+            this.buttonSin.TabIndex = 4;
+            this.buttonSin.Text = "drawSin";
+            this.buttonSin.UseVisualStyleBackColor = true;
+            this.buttonSin.Click += new System.EventHandler(this.buttonSin_Click);
+            // 
+            // buttonRandomFig
+            // 
+            this.buttonRandomFig.Location = new System.Drawing.Point(39, 208);
+            this.buttonRandomFig.Name = "buttonRandomFig";
+            this.buttonRandomFig.Size = new System.Drawing.Size(126, 29);
+            this.buttonRandomFig.TabIndex = 5;
+            this.buttonRandomFig.Text = "drawRandomFig";
+            this.buttonRandomFig.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(884, 602);
+            this.ClientSize = new System.Drawing.Size(1179, 741);
+            this.Controls.Add(this.buttonRandomFig);
+            this.Controls.Add(this.buttonSin);
+            this.Controls.Add(this.buttonCircle);
+            this.Controls.Add(this.buttonTriangle);
+            this.Controls.Add(this.buttonRectangle);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -56,6 +118,11 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonRectangle;
+        private System.Windows.Forms.Button buttonTriangle;
+        private System.Windows.Forms.Button buttonCircle;
+        private System.Windows.Forms.Button buttonSin;
+        private System.Windows.Forms.Button buttonRandomFig;
     }
 }
 
--- neural network/Form1.cs
+++ neural network/Form1.cs
@@ -15,6 +15,136 @@ namespace neural_network
         public Form1()
         {
             InitializeComponent();
+
+            pictureBox1.Width = 200;
+            pictureBox1.Height = 200;
+
+            dist_to_center_to = 20;
+            radius_from = 35;
+            radius_to = 50;
+
+            rand = new Random();
+        }
+
+        private static Graphics g;
+        private static Bitmap bmp;
+        private Random rand;
+
+        int dist_to_center_to, radius_from, radius_to;
+
+        private System.IO.StreamWriter writer = new System.IO.StreamWriter("debug.txt");
+
+        private void clear()
+        {
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(pictureBox1.Image);
+            bmp = (Bitmap)pictureBox1.Image;
+        }
+
+        private void drawLine(Point p1, Point p2)
+        {
+            //g.DrawLine(new Pen(Color.Black), new Point((int)p1.X, (int)p1.Y), new Point((int)p2.X, (int)p2.Y));
+            g.DrawLine(new Pen(Color.Black), p1, p2);
+        }
+
+        private void drawLine(int x1, int y1, int x2, int y2)
+        {
+            g.DrawLine(new Pen(Color.Black), new Point(x1, y1), new Point(x2, y2));
+        }
+
+        private void drawCircle(int x, int y, int w)
+        {
+            g.DrawEllipse(new Pen(Color.Black), x, y, w, w);
+        }
+
+        private void drawSin()
+        {
+            int w = pictureBox1.Width;
+            int h = pictureBox1.Height;
+
+            int width = rand.Next(radius_from, radius_to) * 2;
+            int height = rand.Next(radius_from, radius_to) * 2;
+            int x = w / 2 + rand.Next(-dist_to_center_to, dist_to_center_to) - width / 2;
+            int y = h / 2 + rand.Next(-dist_to_center_to, dist_to_center_to) - height / 2;
+        }
+
+        private void drawRectangle()
+        {
+            int w = pictureBox1.Width;
+            int h = pictureBox1.Height;
+
+            int width = rand.Next(radius_from, radius_to) * 2;
+            int height = rand.Next(radius_from, radius_to) * 2;
+            int x = w / 2 + rand.Next(-dist_to_center_to, dist_to_center_to) - width/2;
+            int y = h / 2 + rand.Next(-dist_to_center_to, dist_to_center_to) - height/2;
+
+            g.DrawRectangle(new Pen(Color.Black), x, y, width, height);
+
+            writer.WriteLine("x = " + x.ToString() + " | y = " + y.ToString() + 
+                " | w = " + width.ToString() + " | h = " + height.ToString());
+            writer.Flush();
+        }
+
+        private void drawTriangle()
+        {
+            int w = pictureBox1.Width;
+            int h = pictureBox1.Height;
+
+            int r = rand.Next(radius_from, radius_to);
+            int x1 = w / 2 + rand.Next(-dist_to_center_to, dist_to_center_to) - r;
+            int x2 = w / 2 + rand.Next(-dist_to_center_to, dist_to_center_to) + r;
+            //int x3 = rand.Next(x1, x2);
+            int x3 = (x2 + x1) / 2;
+            int y1 = h / 2 + rand.Next(-dist_to_center_to, dist_to_center_to) + r;
+            int y2 = y1;
+            int y3 = h / 2 + rand.Next(-dist_to_center_to, dist_to_center_to) - r;
+
+            drawLine(x1, y1, x2, y2);
+            drawLine(x2, y2, x3, y3);
+            drawLine(x3, y3, x1, y1);
+        }
+
+        private void drawCircle()
+        {
+            int w = pictureBox1.Width;
+            int h = pictureBox1.Height;
+
+            int r = rand.Next(radius_from, radius_to);
+            int x = w / 2 + rand.Next(-dist_to_center_to, dist_to_center_to) - r;
+            int y = h / 2 + rand.Next(-dist_to_center_to, dist_to_center_to) - r;
+
+            drawCircle(x, y, r*2);
+
+            writer.WriteLine("x = " + x.ToString() + " | y = " + y.ToString() + " | w = " + (r * 2).ToString());
+            writer.Flush();
+        }
+
+        private void buttonRectangle_Click(object sender, EventArgs e)
+        {
+            clear();
+            drawRectangle();
+            pictureBox1.Image = pictureBox1.Image;
+        }
+
+        private void buttonTriangle_Click(object sender, EventArgs e)
+        {
+            clear();
+            drawTriangle();
+            pictureBox1.Image = pictureBox1.Image;
+        }
+
+        private void buttonCircle_Click(object sender, EventArgs e)
+        {
+            clear();
+            drawCircle();
+            pictureBox1.Image = pictureBox1.Image;
+        }
+
+        private void buttonSin_Click(object sender, EventArgs e)
+        {
+            clear();
+            drawSin();
+            pictureBox1.Image = pictureBox1.Image;
         }
     }
 }
