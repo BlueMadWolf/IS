340c4433a6dce9b85a8402d1442255ccdea8c65a
= <rud.sasha1997@gmail.com>
2018-12-24 20:03:41 +0300

    tgbot upd

---
+++ tgBot/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace tgBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxToken = new System.Windows.Forms.TextBox();
+            this.buttonRunBot = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 229);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // textBoxToken
+            // 
+            this.textBoxToken.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.86792F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxToken.Location = new System.Drawing.Point(12, 38);
+            this.textBoxToken.Name = "textBoxToken";
+            this.textBoxToken.Size = new System.Drawing.Size(460, 26);
+            this.textBoxToken.TabIndex = 1;
+            // 
+            // buttonRunBot
+            // 
+            this.buttonRunBot.Location = new System.Drawing.Point(113, 98);
+            this.buttonRunBot.Name = "buttonRunBot";
+            this.buttonRunBot.Size = new System.Drawing.Size(201, 54);
+            this.buttonRunBot.TabIndex = 2;
+            this.buttonRunBot.Text = "Run bot";
+            this.buttonRunBot.UseVisualStyleBackColor = true;
+            this.buttonRunBot.Click += new System.EventHandler(this.buttonRunBot_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 272);
+            this.Controls.Add(this.buttonRunBot);
+            this.Controls.Add(this.textBoxToken);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxToken;
+        private System.Windows.Forms.Button buttonRunBot;
+    }
+}
+
---
+++ tgBot/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+using Telegram.Bot;
+
+namespace tgBot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static TelegramBotClient Bot;
+        private AIMLbot.Bot aiml;
+        AIMLbot.User me;
+
+        async void InitBot()
+        {
+            string token = textBoxToken.Text;
+
+            WebProxy wp = new WebProxy("142.93.87.88:3128", true);
+            wp.Credentials = new NetworkCredential("user1", "user1password");
+
+            Bot = new TelegramBotClient(token, wp);
+
+            if (!await Bot.TestApiAsync())
+            {
+                throw new Exception("Can't reach Telegram");
+            }
+
+            Bot.OnMessage += Bot_OnMessage;
+            Bot.OnMessageEdited += Bot_OnMessage;
+            Bot.StartReceiving();
+
+            label1.Text = "Run";
+        }       
+
+        private void InitAIML()
+        {
+            aiml = new AIMLbot.Bot();
+            string p1 = Environment.CurrentDirectory;
+            string p2 = Path.Combine("config", "Settings.xml");
+            string p = Path.Combine(p1, p2);
+            aiml.loadSettings();
+            
+            me = new AIMLbot.User("Lucky", aiml);
+            aiml.isAcceptingUserInput = false;
+            aiml.loadAIMLFromFiles();
+            aiml.isAcceptingUserInput = true;
+
+            
+        }
+
+        private void buttonRunBot_Click(object sender, EventArgs e)
+        {
+            InitBot();
+            InitAIML();
+        }
+
+        private void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+           
+            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+            {
+                /*
+                if (e.Message.Text == "How are you?")
+                    Bot.SendTextMessageAsync(e.Message.Chat.Id, "Fine, thank you) And you?");
+                else if (e.Message.Text == "Good morning)")
+                {
+                    Bot.SendTextMessageAsync(e.Message.Chat.Id, "Good morning, " + e.Message.Chat.Username);
+                }
+                else
+                {
+                    Bot.SendTextMessageAsync(e.Message.Chat.Id, @"Usage:                            
+                       How are you?                            
+                       Good morning!
+                        ");
+                }*/
+                AIMLbot.Request r = new AIMLbot.Request(e.Message.Text, me, aiml);
+                AIMLbot.Result res = aiml.Chat(r);
+                Bot.SendTextMessageAsync(e.Message.Chat.Id, res.Output);
+
+                
+
+                //label1.Text += "Received: " + e.Message.Text;
+            }
+        }
+    }
+}
